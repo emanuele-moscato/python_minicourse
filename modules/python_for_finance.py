012362cbcbb180fb51b19a7fec6ab7629cce1da4
Emanuele Moscato <emanuele_moscato@MacBook-Pro-di-Emanuele.local>
2019-12-02 17:34:25 +0100

    Add other plotting functions to module

--- modules/python_for_finance.py
+++ modules/python_for_finance.py
@@ -1,4 +1,5 @@
 import plotly.graph_objs as go
+import numpy as np
 
 
 def plot_histogram(values, title=None, xaxis_title=None):
@@ -24,3 +25,68 @@ def plot_histogram(values, title=None, xaxis_title=None):
     )
 
     return go.Figure(data=[trace], layout=layout)
+
+
+def plot_scatter(x_data, y_data):
+    """
+    Plots 2d datapoints with coordinates in x_data and y_data.
+    """
+    trace = go.Scatter(
+      x=x_data,
+      y=y_data,
+      mode="markers"
+    )
+
+    layout = go.Layout(
+        xaxis=dict(
+          title="x"
+        ),
+        yaxis=dict(
+          title="y"
+        )
+    )
+
+    fig = go.Figure(data=[trace], layout=layout)
+
+    return fig
+
+
+def plot_regression(x_data, y_data, regressor):
+    """
+    Plots 2d datapoints with coordinates in x_data and y_data and overlays
+    predictions given by regressor in the same interval of x values where the
+    data lives.
+    """
+    x_plot = np.linspace(
+        x_data.min(),
+        x_data.max(),
+        1000
+    )
+
+    y_plot = regressor.coef_[0] * x_plot + regressor.intercept_
+
+    trace_data = go.Scatter(
+        x=x_data[:, 0],
+        y=y_data,
+        mode="markers",
+        name="data"
+    )
+
+    trace_fit = go.Scatter(
+        x=x_plot,
+        y=y_plot,
+        name="regression line"
+    )
+
+    layout = go.Layout(
+        xaxis=dict(
+          title="x"
+        ),
+        yaxis=dict(
+          title="y"
+        )
+    )
+
+    fig = go.Figure(data=[trace_data, trace_fit], layout=layout)
+
+    return fig
